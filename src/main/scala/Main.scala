1eac22177929eec1d0ae9b0414ead5a1f860c168
LBotho <loic.bothorel@icloud.com>
2018-03-16 22:23:00 -0400

    Beginning rdd

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,13 +1,17 @@
 import org.apache.spark.SparkConf
 import org.apache.spark.SparkContext
+import org.apache.spark.sql.DataFrame
 
 object Main extends App {
   val appname = "Devoir 2"
   val master = "local"
+  val file = "data.txt"
   val conf = new SparkConf().setAppName(appname).setMaster(master)
   val sc = new SparkContext(conf)
-  val lines = sc.textFile("data.txt")
-  val lineLengths = lines.flatMap(Functions.func1).collect
+  //val data = sc.textFile(file)
+  //read.json(data)
+  val rdd = sc.parallelize(data)
+  //val lineLengths = data.flatMap(Functions.func1)
   //val totalLength = lineLengths.reduce((a, b) => a + b)
 }
 
