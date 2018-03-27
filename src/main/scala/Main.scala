a54b9a92014bd09ad8093fb60920eb6270aa0d4c
Paul <paul.du56@gmail.com>
2018-03-27 16:38:05 -0400

    New way of reading the json file Compute the batch view

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,18 +1,37 @@
 import org.apache.spark.SparkConf
 import org.apache.spark.SparkContext
-import org.apache.spark.sql.SparkSession
+import net.liftweb.json.DefaultFormats
+import net.liftweb.json._
+
+import scala.collection.mutable.ListBuffer
 
 object Main extends App {
+  implicit val formats: DefaultFormats.type = DefaultFormats
+
   val appname = "Devoir 2"
   val master = "local"
   val file = "crawlers/allMonsters.json"
   val conf = new SparkConf().setAppName(appname).setMaster(master)
   val sc = new SparkContext(conf)
-  val sqlContext = SparkSession.builder().master(master).getOrCreate()
 
-  val df = sqlContext.read.json(sc.wholeTextFiles(file).values)
-  df.createOrReplaceTempView("bestiary")
-  val sqlDF = sqlContext.sql("SELECT count(*) FROM bestiary")
-  sqlDF.show()
+
+  val allMonsterString = scala.io.Source.fromFile(file).mkString
+  val json = parse(allMonsterString)
+
+
+  val allMonster = json.children
+  var allMonsterList = new ListBuffer[Monster]()
+  for (acct <- allMonster) {
+    val monster = acct.extract[Monster]
+    allMonsterList+= monster
+  }
+
+  val rddAllMonster = sc.parallelize(allMonsterList)
+  println(rddAllMonster)
+
+  val batchViewSpellMonsters = rddAllMonster.flatMap(monster=>
+    monster.spells.map(spells => (spells, monster.name))
+  ).groupByKey()
+
+  batchViewSpellMonsters.foreach(println)
 }
-
