9072702a9bd9e4b718111106430eacbd307006ec
LBotho <loic.bothorel@icloud.com>
2018-03-27 10:55:23 -0400

    Read json

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,23 +1,16 @@
 import org.apache.spark.SparkConf
 import org.apache.spark.SparkContext
-import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.SparkSession
 
 object Main extends App {
   val appname = "Devoir 2"
   val master = "local"
-  val file = "data.txt"
+  val file = "allMonsters.json"
   val conf = new SparkConf().setAppName(appname).setMaster(master)
   val sc = new SparkContext(conf)
-  //val data = sc.textFile(file)
-  //read.json(data)
-  val rdd = sc.parallelize(data)
-  //val lineLengths = data.flatMap(Functions.func1)
-  //val totalLength = lineLengths.reduce((a, b) => a + b)
-}
+  val sqlContext = SparkSession.builder().master(master).getOrCreate()
+  val df = sqlContext.read.json(file)
+  df.show()
 
-object Functions {
-  def func1(s: String): String = {
-    s
-  }
 }
 
