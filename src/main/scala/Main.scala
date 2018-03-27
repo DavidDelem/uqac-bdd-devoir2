52fd00b7d56262f493fd7caad45a09edc25bd889
LBotho <loic.bothorel@icloud.com>
2018-03-27 11:46:07 -0400

    Read json

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -5,12 +5,16 @@ import org.apache.spark.sql.SparkSession
 object Main extends App {
   val appname = "Devoir 2"
   val master = "local"
-  val file = "allMonsters.json"
+  val file = "crawlers/allMonsters.json"
   val conf = new SparkConf().setAppName(appname).setMaster(master)
   val sc = new SparkContext(conf)
   val sqlContext = SparkSession.builder().master(master).getOrCreate()
-  val df = sqlContext.read.json(file)
-  df.show()
 
+  val rdd = sc.textFile(file)
+  val df = sqlContext.read.json(sc.wholeTextFiles(file).values)
+  df.printSchema()
+//  df.createOrReplaceTempView("bestiary")
+//  val sqlDF = sqlContext.sql("SELECT * FROM bestiary")
+//  sqlDF.show()
 }
 
