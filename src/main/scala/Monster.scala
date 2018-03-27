436d8e0ea3161b7cc0a30fe6282840812eadd82a
Paul <paul.du56@gmail.com>
2018-03-27 16:37:14 -0400

    Class monster to read the json

---
+++ src/main/scala/Monster.scala
@@ -0,0 +1,10 @@
+class Monster extends Serializable {
+  var name = ""
+  var spells = List.empty[String]
+  def this(name:String, spells:List[String]) {
+    this()
+    this.name = name
+    this.spells = spells
+  }
+
+}
