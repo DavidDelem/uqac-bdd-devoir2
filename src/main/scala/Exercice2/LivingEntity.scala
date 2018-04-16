e264b613a9c851a29e4319a82b04eadf48983e0c
Paul <paul.du56@gmail.com>
2018-04-15 21:29:27 -0400

    Fix warnings

--- src/main/scala/Exercice2/LivingEntity.scala
+++ src/main/scala/Exercice2/LivingEntity.scala
@@ -4,7 +4,7 @@ class LivingEntity extends Serializable {
   protected var name = ""
   protected var hp = 0
 
-  def getName():String = {
-    return this.name;
+  def getName:String = {
+    this.name
   }
 }
--- src/main/scala/Exercice2/Monster.scala
+++ src/main/scala/Exercice2/Monster.scala
@@ -6,13 +6,13 @@ class Monster() extends LivingEntity {
   protected var regeneration = 0
 
   //Type de vitesse, vitesse
-  protected var speeds = List.empty[Tuple2[String, Int]]
+  protected var speeds = List.empty[(String, Int)]
 
   //Nom attaque, Précisions, Dommages[Min, Max], Distance à laquelle il peut utiliser l'attaque
-  protected var melee = ("", List.empty[Int], List.empty[Int], 0)
+  protected var melee: (String, List[Int], List[Int], Int) = ("", List.empty[Int], List.empty[Int], 0)
 
   //Nom attaque, Précisions, Dommages[Min, Max],  Distance à laquelle il peut utiliser l'attaque
-  protected var ranged = ("", List.empty[Int], List.empty[Int], 0)
+  protected var ranged: (String, List[Int], List[Int], Int) = ("", List.empty[Int], List.empty[Int], 0)
 
   override def toString: String = "Name : " + name + ", HP : " + hp + ", Armor : " + armor + ", Regeneration : " + regeneration + "Speeds : " + speeds
 
