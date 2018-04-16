37371f792555c5128e1990b20732a7546012eb46
Paul <paul.du56@gmail.com>
2018-04-15 21:26:31 -0400

    Create graph, relationship and display triplet

--- src/main/scala/Exercice2/Combat1.scala
+++ src/main/scala/Exercice2/Combat1.scala
@@ -1,5 +1,6 @@
 package Exercice2
 
+import Exercice2.Bestiary._
 import org.apache.spark.graphx.{Edge, Graph, VertexId}
 import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkConf, SparkContext}
@@ -12,33 +13,54 @@ object Combat1 extends App {
   val sc = new SparkContext(conf)
   sc.setLogLevel("ERROR")
 
-  val sol = new Solar()
-  print(sol)
 
-
+  //https://spark.apache.org/docs/2.1.1/graphx-programming-guide.html
   // Create an RDD for the vertices
-  val users: RDD[(VertexId, (Monster))] =
+  val protagonist: RDD[(VertexId, (LivingEntity))] =
     sc.parallelize(Array(
-      (1L, new Solar())
+      (1L, new Solar()),
+      (2L, new Pito()),
+      (3L, new OrcWorgRider()),
+      (4L, new OrcWorgRider()),
+      (5L, new OrcWorgRider()),
+      (6L, new OrcWorgRider()),
+      (7L, new OrcWorgRider()),
+      (8L, new OrcWorgRider()),
+      (9L, new OrcWorgRider()),
+      (10L, new OrcWorgRider()),
+      (11L, new OrcWorgRider()),
+      (12L, new BrutalWarlord()),
+      (13L, new DoubleAxeFury()),
+      (14L, new DoubleAxeFury()),
+      (15L, new DoubleAxeFury()),
+      (16L, new DoubleAxeFury())
+
     ))
-//
-//  // Create an RDD for edges
-//  val relationships: RDD[Edge[String]] =
-//    sc.parallelize(Array(
-//      Edge(3L, 7L, "collab"),
-//      Edge(5L, 3L, "advisor"),
-//      Edge(2L, 5L, "colleague"),
-//      Edge(5L, 7L, "pi")
-//    ))
-//  // Define a default user in case there are relationship with missing user
-//  val defaultUser = ("John Doe", "Missing")
-//
-//  // Build the initial Graph
-//  val graph = Graph(users, relationships, defaultUser)
-//
-//  val facts: RDD[String] =
-//    graph.triplets.map(triplet =>
-//      triplet.srcAttr._1 + " is the " + triplet.attr + " of " + triplet.dstAttr._1)
-//  facts.collect.foreach(println(_))
+
+  val relationships: RDD[Edge[String]] =
+    sc.parallelize(Array(
+      Edge(1L, 3L, "enemy"),
+      Edge(1L, 4L, "enemy"),
+      Edge(1L, 5L, "enemy"),
+      Edge(1L, 6L, "enemy"),
+      Edge(1L, 7L, "enemy"),
+      Edge(1L, 8L, "enemy"),
+      Edge(1L, 9L, "enemy"),
+      Edge(1L, 10L, "enemy"),
+      Edge(1L, 11L, "enemy"),
+      Edge(1L, 12L, "enemy"),
+      Edge(1L, 13L, "enemy"),
+      Edge(1L, 14L, "enemy"),
+      Edge(1L, 15L, "enemy"),
+      Edge(1L, 16L, "enemy")
+    ))
+
+  // Build the initial Graph
+  val graph = Graph(protagonist, relationships)
+
+  val facts: RDD[String] =
+    graph.triplets.map(triplet =>
+      triplet.srcAttr.getName() + " is the " + triplet.attr + " of " + triplet.dstAttr.getName())
+  facts.collect.foreach(println(_))
 
 }
