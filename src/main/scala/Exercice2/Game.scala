ecf0e94ec438e98ce669ca90f65c9b4bfceda8a7
Paul <paul.du56@gmail.com>
2018-04-17 13:47:14 -0400

    Update but still not working

--- src/main/scala/Exercice2/Game.scala
+++ src/main/scala/Exercice2/Game.scala
@@ -1,10 +1,37 @@
 package Exercice2
 
 import org.apache.spark.SparkContext
-import org.apache.spark.graphx.{Graph, TripletFields}
+import org.apache.spark.graphx.{EdgeContext, Graph, TripletFields, VertexId}
 
-class Game {
-  //def execute(g: Graph[Link, String], sc: SparkContext): Graph[Link, String] = {
+class Game extends Serializable {
+
+  def sendMsg(ctx: EdgeContext[LivingEntity, Link, LivingEntity]): Unit = {
+    System.out.println("------------- sendMsg -------------")
+//    System.out.println(ctx.srcAttr)
+//    System.out.println(ctx.dstAttr)
+//    System.out.println(ctx.attr.getRelation)
+
+    //faire une liste de toutes les cibles possibles
+    //seulement le solar attaque pour le moment
+    if(ctx.attr.relation == "enemy") {
+      System.out.println("enemy found")
+
+      ctx.sendToDst(ctx.srcAttr)
+      ctx.sendToDst(ctx.dstAttr)
+
+    }
+
+
+
+  }
+
+  def mergeMsg(monster1: LivingEntity, monster2: LivingEntity): LivingEntity = {
+    //choisir parmis toutes les cibles une à attaquer
+    System.out.println("------------- mergeMsg -------------")
+
+    monster1
+  }
+
   def execute(g: Graph[LivingEntity, Link], sc: SparkContext, maxIterations: Int): Graph[LivingEntity, Link] = {
     var myGraph = g
     var roundCounter = 0
@@ -15,9 +42,33 @@ class Game {
         roundCounter+=1
         println("Battle round : " + roundCounter)
 
+        val messages = myGraph.aggregateMessages[LivingEntity](
+          sendMsg,
+          mergeMsg,
+          fields
+        )
+
+        if (messages.isEmpty()) {
+          System.out.println("Did nothing")
+        }
+
+        myGraph = myGraph.joinVertices(messages) {
+          System.out.println("------------- join vertices -------------")
+          //faire la perte des hp
+          System.out.println(messages.collect().foreach(elem => println(elem)))
+          (vid, sommet, bestId) => sommet
+        }
+
+
+
+        //Ignorez : Code de debug
+//        var printedGraph = myGraph.vertices.collect()
+//        printedGraph = printedGraph.sortBy(_._1)
+//        printedGraph.foreach(
+//          elem => println(elem._2)
+//        )
+
         if (roundCounter == maxIterations) return
-
-
 
 
       }
