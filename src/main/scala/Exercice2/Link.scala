d76e7c1cc7cc14519f1a633f914658dc9bd28ee2
Paul <paul.du56@gmail.com>
2018-04-17 13:46:12 -0400

    Remove useless getter & setter

--- src/main/scala/Exercice2/Link.scala
+++ src/main/scala/Exercice2/Link.scala
@@ -1,21 +1,5 @@
 package Exercice2
 
-class Link (var relation: String = "", var distance: Int = 200) extends Serializable {
+class Link (var relation: String = "") extends Serializable {
 
-
-  def setRelation(newRelation:String) = {
-    this.relation = newRelation
-  }
-
-  def getRelation:String = {
-    this.relation
-  }
-
-  def setDistane(newDistance:Int) = {
-    this.distance = newDistance
-  }
-
-  def getDistance:Int = {
-    this.distance
-  }
 }
